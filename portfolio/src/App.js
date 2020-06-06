a6f6567a28a376e4f224fa40db2b79fae7fe1bba
Hunter Matthews <sirhunty@gmail.com>
2020-06-06 12:14:37 -0700

    set up initial state for page routes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+//utilizing class-based component inrder to use state
+class App extends React.Component {
+
+  //state for top level elements on site (title in navbar and links, home, about, contact pages)
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Christopher H Matthews",
+      headerLinks: [
+        { title: "Home", path: "/" },
+        { title: "About Me", path: "/about" },
+        { title: "My Projects", path: "/projects" },
+        { title: "My Art", path: "/art" },
+        { title: "Contact", path: "/contact" }
+      ],
+      home: {
+        title: "Christopher Matthews",
+        subTitle: "Web Developer & Graphic Designer",
+      },
+      about: {
+        title: "BIO",
+      },
+      projects: {
+        title: "My Projects",
+      },
+      art: {
+        title: "My Art",
+      },
+      contact: {
+        title: "Stay Connected with Me!",
+        subTitle: "Leave Me a Note"
+      }
+    }
+  }
+
+
+  render() {
+    return (
+      <div>My React Portfolio</div>
   );
+  }
 }
 
 export default App;
