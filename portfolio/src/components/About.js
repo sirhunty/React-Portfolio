3b1ad7061f306dc60c0de555ed1d96343e24d76b
Hunter Matthews <sirhunty@gmail.com>
2020-06-25 15:23:16 -0700

    updated the main js page

---
+++ portfolio/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+function About() {
+    return (
+        <div>
+            <main>
+                <section class="home">
+                    <p>Test P tag</p>
+                    <footer>&copy; 2020 Christopher H Matthews</footer>
+                </section>
+            </main>
+        </div>
+    );
+}
+export default About;
