3cddd2a4d884b16b66c1085fdccf5d03898cd746
Hunter Matthews <sirhunty@gmail.com>
2020-06-30 07:19:31 -0700

    added about me page; having issues with skills list no longer vertical

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -3,12 +3,177 @@ function About() {
     return (
         <div>
             <main>
-                <section class="home">
-                    <p>Test P tag</p>
-                    <footer>&copy; 2020 Christopher H Matthews</footer>
-                </section>
-            </main>
-        </div>
+        <section className="about">
+            <div className="about__bio-image">
+                <div className="about__bio">
+                    <h1>BIO</h1>
+                    <p>Web Developer experienced in Agile development deliveries. I can successfully maintain front and
+                        back end existing code and create web applications. Within my last role, I successfully
+                        maintained multiple product account configurations and support channels for internal and
+                        external use. What motivates me as a web developer is to become a part of a team where I can
+                        utilize my knowledge and skills while learning from my peers. I have a Bachelor’s in Graphic
+                        Design and earned a certificate in Full Stack Development from the University of Arizona. Skills
+                        attained from this course are React, MySQL, and Node to name a few. I feel the combination of my
+                        technical expertise, passion to learn, and leadership experience makes me an advantageous
+                        candidate for any development team.</p>
+                </div>
+            </div>
+
+            <div className="skills">
+                <div className="skills-list">
+                    <h1>Acquired Skills</h1>
+                </div>
+                <div class="skills__skill">
+                    <i className="devicon-html5-plain-wordmark colored"></i>
+                    <i className="devicon-css3-plain-wordmark colored"></i>
+                    <i className="devicon-javascript-plain colored"></i>
+                    <i className="devicon-react-original-wordmark colored"></i>
+                    <i className="devicon-heroku-line-wordmark"></i>
+                    <i className="devicon-bootstrap-plain-wordmark"></i>
+                    <i className="devicon-visualstudio-plain-wordmark"></i>
+                    <i className="devicon-git-plain-wordmark colored"></i>
+                    <i className="devicon-github-plain-wordmark"></i>
+                    <i className="devicon-gitlab-plain-wordmark colored"></i>
+                    <i className="devicon-jquery-plain-wordmark colored"></i>
+                    <i className="devicon-mongodb-plain-wordmark colored"></i>
+                    <i className="devicon-mysql-plain-wordmark"></i>
+                    <i className="devicon-nodejs-plain-wordmark colored"></i>
+                    <i className="devicon-photoshop-line colored"></i>
+                    <i className="devicon-sequelize-plain-wordmark"></i>
+                    <i className="devicon-express-original-wordmark"></i>
+                    <i className="devicon-slack-plain-wordmark"></i>
+                </div>
+            </div>
+
+            <div className="jobs">
+                <div className="work-history">
+                    <h1>Work History</h1>
+                </div>
+                <div className="jobs__job">
+                    <h2>Jan 2017 - Mar 2020</h2>
+                    <h3>Digital Air Strike</h3>
+                    <h5>Activations and Support</h5>
+                    <ul className="job-desc-list">
+                    <br></br>
+                        <li className="job-desc">
+                            Onboarded and maintained between 50 and 100 client products and supported any issues going
+                            forward which allowed for a 10% increase in customer retention.
+                        </li>
+                        <li className="job-desc">
+                            Recognized for completing a full audit process of all accounts within a particular program,
+                            and awarded for the responsiveness and efficiency of the report.
+                        </li>
+                    </ul>
+                </div>
+                <div className="jobs__job">
+                    <h2>Oct 2016 - Jan 2017</h2>
+                    <h3>Logicalis</h3>
+                    <h5>Service Desk Technician</h5>
+                    <ul className="job-desc-list">
+                    <br></br>
+                        <li className="job-desc">
+                            Created support tickets for customer issues by logging detailed information and researching
+                            solutions.
+                        </li>
+                        <li className="job-desc">
+                            Communicated with customers primarily over the phone, remotely, or even chat/email.
+                        </li>
+                        <li className="job-desc">
+                            Developed patience and attention to detail while managing customer accounts and building
+                            relationships.
+                        </li>
+                    </ul>
+                </div>
+                <div className="jobs__job">
+                    <h2>Jul 2014 - Oct 2016</h2>
+                    <h3>Dutch Bros. Coffee</h3>
+                    <h5>Assistant Manager</h5>
+                    <ul className="job-desc-list">
+                    <br></br>
+                        <li className="job-desc">
+                            Recognized for my drive and dedication to the team and was promoted to an assistant manager.
+                        </li>
+                        <li className="job-desc">
+                            Charged with customer service, product management, and completing two full product
+                            deliveries each week.
+                        </li>
+                        <li className="job-desc">
+                            Learned to communicate with people and have genuine moments, while understanding how to
+                            manage people and products.
+                        </li>
+                    </ul>
+                </div>
+                <div className="jobs__job">
+                    <h2>Jan 2012 - Jul 2013</h2>
+                    <h3>SAP Retail Labs</h3>
+                    <h5>Quality Assurance</h5>
+                    <ul className="job-desc-list">
+                    <br></br>
+                        <li className="job-desc">
+                            Gained AGILE Development and daily SCRUM team meeting experience.
+                        </li>
+                        <li className="job-desc">
+                            Understand the lifecycle of development from both the testing side and now the development
+                            side.
+                        </li>
+                        <li className="job-desc">
+                            Recognized for completing many audits for different development teams.
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="schools">
+                    <div className="education">
+                        <h1>Education</h1>
+                    </div>
+                    <div className="schools__school">
+                        <h2>Oct 2019 - May 2020</h2>
+                        <h3>Univeristy of Arizona</h3>
+                        <h5>Full Stack Web Development Certificate</h5>
+                        <ul className="schools-desc-list">
+                        <br></br>
+                            <li className="school-desc">
+                                A 24-week intensive program focused on gaining technical programming skills in HTML5,
+                                CSS3, Javascript, JQuery, Bootstrap, Firebase, Node.js, MySQL, MongoDB, Express,
+                                Handelbars.js & React.js.
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="schools__school">
+                        <h2>Aug 2011 - Dec 2014</h2>
+                        <h3>Arizona State Univeristy</h3>
+                        <h5>Bachelor's Degree in Digital Culture and Graphic Design</h5>
+                        <ul className="schools-desc-list">
+                        <br></br>
+                            <li className="school-desc">
+                                Focus in Digital Design and Graphics with emphasis on Adobe Suite Products and other 3D
+                                programs such as Cinema 4D and Rhino. Also touched on basics of web development with
+                                HTML, CSS, JavaScript, and Processing.
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="schools__school">
+                        <h2>Aug 2009 - May 2011</h2>
+                        <h3>Northern Arizona University</h3>
+                        <h5>Graphic Design</h5>
+                        <ul className="schools-desc-list">
+                        <br></br>
+                            <li className="school-desc">
+                                Adobe Creative Suite
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+
+            </div>
+
+            
+
+            <footer>&copy; 2020 Christopher H Matthews</footer>
+        </section>
+    </main>
+</div>
     );
 }
 export default About;
